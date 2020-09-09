d29667384d586f8cb3c1d6c826543ed676e4c5dc
Zachariassis <Zachariassis@gmail.com>
2020-09-09 00:35:09 -0500

    Finished first part for Weather

--- api_keys.py
+++ api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE!"
+weather_api_key = "6298ae5352ff8dc0cfb22ee721b10df4"
 
 # Google API Key
-g_key = "YOUR KEY HERE!"
+g_key = "AIzaSyA6rXrVBcQ37SoaiF-p10va1B1jna4qq7Q"
